6c90556209a959d1460f8c4aa12ce85fe7605783
Gurnelius <terergurnelius@gmail.com>
2023-02-16 02:18:55 -0800

    Update script

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,11 @@
-#!/bin/bash
 #include <unistd.h>
+
+/**
+ * main - Entry point
+ * Return: 1 Always
+ */
+int main(void)
+{
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
