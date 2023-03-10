ecee382e76251c23bb5bc9beb8c7078eaabbc9b1
Gurnelius <terergurnelius@gmail.com>
2023-03-10 11:13:58 -0800

    Init commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints its name, followed by a new line
+ * @argc: argument counts
+ * @argv: arguments array
+ *
+ * Return: always 0
+ */
+int main(int __attribute__((__unused__)) argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
