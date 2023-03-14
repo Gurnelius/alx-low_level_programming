291deb8dd6b65e9eec3d1fdf71048ef6094577c8
Gurnelius <terergurnelius@gmail.com>
2023-03-14 01:08:49 -0700

    Init commit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -7,11 +7,11 @@ char *str_concat(char *s1, char *s2)
 	unsigned int j = 0;
 	char *str;
 	
-	if (s1 == NULL)
+	if (s1 == NULL || s2 == NULL)
+	{
 		s1[0] = '\0';
-
-	if (s2 == NULL)
 		s2[0] = '\0';
+	}
 
 	while (s1[i] != '\0')
 		i++;
