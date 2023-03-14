7674bdcdca2ca64948905192a49c306d46487a53
Gurnelius <terergurnelius@gmail.com>
2023-03-14 12:43:31 -0700

    Init commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -34,21 +34,19 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == NULL)
 		{
-			for (i--; i >= 0; i--)
-				free(arr[i]);
-			free(arr);
+			free(arr[i]);
 			return (NULL);
 		}
 	}
+
+	if (arr == NULL)
+		return (NULL);
 
 	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			arr[i][j] = 0;
 
-	if (arr == NULL)
-		return (NULL);
 	return (arr);
 }
