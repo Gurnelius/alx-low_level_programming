ea9646388525271d81ed1d1e555773f7548970ef
Gurnelius <terergurnelius@gmail.com>
2023-03-16 22:13:00 +0300

    Init commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,49 +1,53 @@
-#include "main.h"
 #include <stdlib.h>
+#include <stdlib.h>
+#include "main.h"
 /**
- * string_nconcat - concatenates two strings
- * @s1: first string
- * @s2: second string
- * @n: number of bytes to copy from 2nd string
- *
- * Return: pointer to combined string
+ * _strlen - calculate and return string length
+ * @string: string
+ * Return: string length
+ */
+int _strlen(char *string)
+{
+	int i;
+
+	for (i = 0; string[i] != '\0'; i++)
+		;
+	return (i);
+}
+/**
+ * string_nconcat - concatenate s1 and n bytes of s2; return ptr to string
+ * @s1: string 1
+ * @s2: string 2
+ * @n: n bytes to concat from string 2
+ * Return: pointer to concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *ptr;
-	unsigned int size = 0;
-	unsigned int i = 0;
-	unsigned int j = 0;
+	int num, len, i, j;
 
-	while (s1[i] != '\0')
-	{
-		size++;
-		i++;
-	}
-	while (j < n && s2[j] != '\0')
-	{
-		size++;
-		j++;
-	}
-	ptr = malloc(sizeof(char) * size + 1);
-	if (ptr == NULL)
-	{
-		free(ptr);
+	num = n;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	if (num < 0)
 		return (NULL);
-	}
-	i = 0;
-	while (s1[i] != '\0')
-	{
+	if (num >= _strlen(s2))
+		num = _strlen(s2);
+
+	len = _strlen(s1) + num + 1;
+
+	ptr = malloc(sizeof(*ptr) * len);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
 		ptr[i] = s1[i];
-		i++;
-	}
-	j = 0;
-	while (j < n && s2[j] != '\0')
-	{
-		ptr[i] = s2[j];
-		j++;
-		i++;
-	}
-	ptr[i] = '\0';
+	for (j = 0; j < num; j++)
+		ptr[i + j] = s2[j];
+	ptr[i + j] = '\0';
+
 	return (ptr);
 }
