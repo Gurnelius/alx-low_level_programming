2ff3d8bfd79232096e18cbd54c8c4828c42ffcc0
Gurnelius <terergurnelius@gmail.com>
2023-03-16 21:20:40 +0300

    Init commit

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes to copy from 2nd string
+ *
+ * Return: pointer to combined string
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ptr;
+	unsigned int size = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
+
+	while (s1[i] != '\0')
+	{
+		size++;
+		i++;
+	}
+	size +=	n;
+	ptr = malloc(sizeof(int) * size);
+	if (ptr == NULL)
+	{
+		free(ptr);
+		return (NULL);
+	}
+	i = 0;
+	while (s1[i] != '\0')
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (j < n)
+	{
+		ptr[i] = s2[j];
+		j++;
+		i++;
+	}
+	return (ptr);
+}
