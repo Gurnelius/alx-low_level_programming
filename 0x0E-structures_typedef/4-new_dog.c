5fd2b9bf138e764dd803727af5c3ef9c03dab0ab
Gurnelius <terergurnelius@gmail.com>
2023-03-20 22:06:43 +0300

    Init commit

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "dog.h"
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *my_dog;
+	
+	my_dog = malloc(sizeof(my_dog));
+	if(my_dog == NULL)
+		return (NULL);
+	my_dog->name = name;
+	my_dog->age = age;
+	my_dog->owner = owner;
+
+	return (my_dog);
+}
