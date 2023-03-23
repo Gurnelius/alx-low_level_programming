abba6c52c1038d18745fe3fc2396eda9a117dea5
Gurnelius <terergurnelius@gmail.com>
2023-03-23 19:42:48 +0300

    Init commit

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list ptr;
+	int sum = 0;
+	unsigned int i;
+	
+	if (n == 0)
+		return 0;
+
+	va_start(ptr, n);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(ptr, int);
+	}
+	va_end(ptr);
+	return sum;
+}
