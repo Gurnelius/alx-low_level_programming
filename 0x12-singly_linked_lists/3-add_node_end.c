756517dd0bd99c1c5da49dc5c5878e20db018b0f
Gurnelius <terergurnelius@gmail.com>
2023-03-30 18:17:12 +0300

    Update script

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,25 +13,20 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *temp_node;
 
 	node = malloc(sizeof(list_t));
+	if (node == NULL)
+		return (NULL);
+
 	node->str = strdup(str);
 	node->len = _strlen(str);
 	node->next = NULL;
 	temp_node = *head;
 	if (*head == NULL)
-	{
 		*head = node;
-		return (node);
-	}
-	while (temp_node->next != NULL)
+	else
 	{
-		temp_node = temp_node->next;
-	}
-	temp_node->next = node;
-
-	if (node == NULL)
-	{
-		free(node);
-		return (NULL);
+		while (temp_node->next != NULL)
+			temp_node = temp_node->next;
+		temp_node->next = node;
 	}
 	return (node);
 }
