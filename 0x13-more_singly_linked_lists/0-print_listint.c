683eed2378f7d1c653069f3b43d245e4fef7f9e6
Gurnelius <terergurnelius@gmail.com>
2023-04-03 10:10:54 +0300

    Update script

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+*print_listint - Prints integers stored in
+*in a linked list
+*
+*@h: pointer to list
+*
+*Return: size of the list
+*/
+size_t print_listint(const listint_t *h)
+{
+	int size = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		size++;
+		h = h->next;
+	}
+	return (size);
+}
