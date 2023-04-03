3196f43a968685f8110f66dadbbe15af9fa0e4c8
Gurnelius <terergurnelius@gmail.com>
2023-04-03 13:18:43 +0300

    Update script

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+*sum_listint - get the sum of elements in list
+*@head: pointer to the list
+*
+*Returns: sum of the elements
+*/
+int sum_listint(listint_t *head)
+{
+	int sum = 0;
+
+	while (head != NULL)
+	{
+		sum += head->n;
+		head = head->next;
+	}
+
+	return (sum);
+}
