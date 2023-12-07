6d6b8e240ed0d1dab835f5e48ef25a1487ed1932
Gurnelius <terergurnelius@gmail.com>
2023-12-07 17:33:01 +0300

    Fix minor issue

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -17,7 +17,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node = malloc(sizeof(dlistint_t));
 
-	if (new_node == NULL || (*h) == NULL)
+	if (new_node == NULL)
 	{
 		free(new_node);
 		return (NULL);
