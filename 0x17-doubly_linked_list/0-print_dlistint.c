2098398d1cdc3a540eb5182fa0a92c920e4d6aa4
Gurnelius <terergurnelius@gmail.com>
2023-12-07 11:53:31 +0300

    Update Betty style

--- 0x17-doubly_linked_list/0-print_dlistint.c
+++ 0x17-doubly_linked_list/0-print_dlistint.c
@@ -2,7 +2,7 @@
 #include "lists.h"
 
 /**
- * print_dlistint - print the elements 
+ * print_dlistint - print the elements
  * of a doubly linked list.
  *
  * @h: pointer to the head of the list
@@ -14,7 +14,7 @@ size_t print_dlistint(const dlistint_t *h)
 	size_t count = 0;
 	const dlistint_t *current = h;
 
-	while(current)
+	while (current)
 	{
 		count += 1;
 		printf("%d\n", current->n);
