d399d3a844a54bed7cc99987e70bbe19cdbb3c13
Gurnelius <terergurnelius@gmail.com>
2023-12-11 14:00:36 +0300

    Remove cleanup

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Compile all .c files into object files
 gcc -Wall -fPIC -c *.c
-
-# Create a dynamic library from the object files
 gcc -shared -o liball.so *.o
-
-# Clean up - remove the object files
-rm *.o
-
