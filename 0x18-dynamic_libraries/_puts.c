4ec0d0adbb442711ff509d2d337c7ce09c397cef
Gurnelius <terergurnelius@gmail.com>
2023-12-11 13:02:50 +0300

    Initial commit

---
+++ 0x18-dynamic_libraries/_puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+*main - Entry point of the program.
+*
+*
+*Return: return 0 always
+*/
+int main(void)
+{
+
+	char displayed[52];
+
+	strcpy(displayed, "\"Programming is like building a multilingual puzzle");
+	puts(displayed);
+	return (0);
+}
