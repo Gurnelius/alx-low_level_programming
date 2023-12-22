8ae12f23a944c78deb5291e3ea2519b2dfc87d88
Gurnelius <terergurnelius@gmail.com>
2023-12-22 23:49:35 +0300

    Fix minor issue

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,7 +41,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	node->value = strdup(value);
 	if (node->value == NULL)
 	{
-		node->value = '\0';
+		node->value = "";
 	}
 
 	if (ht->array[index] == NULL)
