5ffca3ab9ca1d793e287e128d50cbad3d896ad2b
Gurnelius <terergurnelius@gmail.com>
2023-12-22 21:19:12 +0300

    Initial commit

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - print a hash table
+ * @ht: pointer to the hash table
+ *
+ * Return: nothing
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+
+	if (!ht || !ht->array)
+	{
+		printf("{}");
+		return;
+	}
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+
+		while (node != NULL)
+		{
+			printf("'%s': '%s'", node->key, node->value);
+
+			if (node->next != NULL || i < ht->size - 1)
+				printf(", ");
+			node = node->next;
+		}
+	}
+	printf("}\n");
+}
