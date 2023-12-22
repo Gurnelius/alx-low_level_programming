3622444728706b8ea49e53a388ff1f37188314c1
Gurnelius <terergurnelius@gmail.com>
2023-12-23 00:05:25 +0300

    Fix minor issues

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -34,11 +34,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(node);
 		return (0);
 	}
-
-	node->value = strdup(value);
-	if (node->value == NULL)
-		node->value = "";
-
+	node->value = (value != NULL) ? strdup(value) : NULL;
+	if (node->value == NULL && value != NULL)
+	{
+		free(node->key);
+		free(node);
+		return (0);
+	}
 	if (ht->array[index] == NULL)
 	{
 		ht->array[index] = node;
