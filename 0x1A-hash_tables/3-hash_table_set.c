bc1adec9c203816e93ba9e023baea052e6a63a6d
Gurnelius <terergurnelius@gmail.com>
2023-12-22 23:46:36 +0300

    Fix minor issue

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -14,14 +14,21 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (!ht || !ht->array)
+	if (!ht)
+	{
+		free(ht->array);
+		free(ht);
 		return (0);
+	}
 
 	index = key_index((const unsigned char *)key, ht->size);
 	node = malloc(sizeof(hash_node_t));
 
 	if (node == NULL)
+	{
+		free(node);
 		return (0);
+	}
 
 	node->next = NULL;
 	node->key = strdup(key);
