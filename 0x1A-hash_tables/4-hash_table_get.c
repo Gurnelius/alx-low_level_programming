054cab9d9681c76115bdc5d445b42e3a38a3bfa6
Gurnelius <terergurnelius@gmail.com>
2023-12-22 20:47:57 +0300

    Cover edge cases

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -11,21 +11,27 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	char *value;
 	unsigned long int index;
 	hash_node_t *node;
 
-	if (!ht || !ht->array || !key)
+	if (!ht || !ht->array || !key || strlen(key) == 0)
 		return (NULL);
 
 	index = key_index((const unsigned char *)key, ht->size);
 
 	node = ht->array[index];
 
-	if (!node || !node->key)
+	if (!node)
 		return (NULL);
 
-	value = node->value;
+	if (strcmp(node->key, key) == 0)
+		return (node->value);
+	while (node)
+	{
+		node = node->next;
+		if (strcmp(node->key, key) == 0)
+			return (node->value);
+	}
 
-	return (value);
+	return (NULL);
 }
